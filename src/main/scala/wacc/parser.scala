9cccc6c804402c0d2f3e406d915cda71257dac1c
Ruchit Tandon <ruchit10don@gmail.com>
2023-02-10 11:31:12 +0000

    style changes to parser

--- src/main/scala/wacc/parser.scala
+++ src/main/scala/wacc/parser.scala
@@ -28,21 +28,21 @@ object parser {
 
   lazy val expr: Parsley[Expr] =
     precedence(atomExpr, OPENPAREN ~> expr <~ CLOSEDPAREN)(
-      Ops(Prefix)(NotExpr <# "!".label("unary op"), NegExpr <# NEGATE.label("unary op"),
-        LenExpr <# LEN.label("unary op"), OrdExpr <# ORD.label("unary op"),
-        ChrExpr <# CHR.label("unary op")),
-      Ops(InfixL)(ModExpr <# ("%").label("binary op"),
-        DivExpr <# ("/").label("binary op"), MulExpr <# "*".label("binary op")),
-      Ops(InfixL)(AddExpr <# ("+").label("binary op"),
-        SubExpr <# ("-").label("binary op")),
-      Ops(InfixL)(attempt(GTEQExpr <# (">=").label("comparison op")),
-        GTExpr <# (">").label("comparison op"),
-        attempt(LTEQExpr <# ("<=").label("comparison op")),
-        LTExpr <# ("<").label("comparison op")),
-      Ops(InfixL)(EQExpr <# ("==").label("comparison op"),
-        NEQExpr <# ("!=").label("comparison op")),
-      Ops(InfixL)(AndExpr <# ("&&").label("logical op")),
-      Ops(InfixL)(OrExpr <# ("||").label("logical op"))
+      Ops(Prefix)(NotExpr <# "!".label("unary_op"), NegExpr <# NEGATE.label("unary_op"),
+        LenExpr <# LEN.label("unary_op"), OrdExpr <# ORD.label("unary_op"),
+        ChrExpr <# CHR.label("unary_op")),
+      Ops(InfixL)(ModExpr <# ("%").label("binary_op"),
+        DivExpr <# ("/").label("binary_op"), MulExpr <# "*".label("binary_op")),
+      Ops(InfixL)(AddExpr <# ("+").label("binary_op"),
+        SubExpr <# ("-").label("binary_op")),
+      Ops(InfixL)(attempt(GTEQExpr <# (">=").label("comparison_op")),
+        GTExpr <# (">").label("comparison_op"),
+        attempt(LTEQExpr <# ("<=").label("comparison_op")),
+        LTExpr <# ("<").label("comparison_op")),
+      Ops(InfixL)(EQExpr <# ("==").label("comparison_op"),
+        NEQExpr <# ("!=").label("comparison_op")),
+      Ops(InfixL)(AndExpr <# ("&&").label("logical_op")),
+      Ops(InfixL)(OrExpr <# ("||").label("logical_op"))
     ).label("expression")
       .explain("--> Expressions are atomic literals preceded by or followed by" +
         " unary, binary, logical and comparison operators")
@@ -64,25 +64,25 @@ object parser {
   val baseType: Parsley[BaseType] = ((IntType <# "int") <|>
     (BoolType <# "bool") <|>
     (CharType <# "char") <|>
-    (StringType <# "string")).label("primitive base type")
+    (StringType <# "string")).label("primitive_base_type")
   lazy val arrayType: Parsley[ArrayType] = chain.postfix1((baseType <|> pairType), (OPENSQUAREBRAC ~> CLOSESQUAREBRAC) #> ArrayType)
   val pairType: Parsley[Type] = PairType(PAIR ~> OPENPAREN ~> pairelemType, COMMA ~> pairelemType <~ CLOSEDPAREN)
-  lazy val pairelemType: Parsley[PairElemType] = attempt(arrayType) <|> baseType <|> (PAIR #> DummyPair)
+  lazy val pairelemType: Parsley[PairElemType] = attempt(arrayType) <|> baseType <|> (DummyPair <# PAIR)
 
   // Statement Parsers
-  val skip: Parsley[Statement] = Skip <# "skip".label("Statement beginning")
+  val skip: Parsley[Statement] = Skip <# "skip".label("Statement_beginning")
   val vardec: Parsley[Statement] = VarDec(waccType, IDENT, ("=" ~> rvalue))
   val assign: Parsley[Statement] = Assign(lvalue, ("=" ~> rvalue))
-  val read: Parsley[Statement] = Read(READ.label("Statement beginning") ~> lvalue)
-  val free: Parsley[Statement] = Free(FREE.label("Statement beginning") ~> expr)
-  val print: Parsley[Statement] = Print(PRINT.label("Statement beginning") ~> expr)
-  val println: Parsley[Statement] = Println(PRINTLN.label("Statement beginning") ~> expr)
-  val ifStat: Parsley[Statement] = If((IF.label("Statement beginning") ~> expr), (THEN ~> statement), (ELSE ~> statement <~ FI))
-  val whileStat: Parsley[Statement] = While((WHILE.label("Statement beginning") ~> expr), (DO ~> statement <~ DONE))
-  val scopeStat: Parsley[Statement] = ScopeStat(BEGIN.label("Statement beginning") ~> statement <~ END)
+  val read: Parsley[Statement] = Read(READ.label("Statement_beginning") ~> lvalue)
+  val free: Parsley[Statement] = Free(FREE.label("Statement_beginning") ~> expr)
+  val print: Parsley[Statement] = Print(PRINT.label("Statement_beginning") ~> expr)
+  val println: Parsley[Statement] = Println(PRINTLN.label("Statement_beginning") ~> expr)
+  val ifStat: Parsley[Statement] = If((IF.label("Statement_beginning") ~> expr), (THEN ~> statement), (ELSE ~> statement <~ FI))
+  val whileStat: Parsley[Statement] = While((WHILE.label("Statement_beginning") ~> expr), (DO ~> statement <~ DONE))
+  val scopeStat: Parsley[Statement] = ScopeStat(BEGIN.label("Statement_beginning") ~> statement <~ END)
 
   // terminal statements
-  lazy val terminalStat: Parsley[Statement] = (returnStat <|> exit).label("return/exit statement")
+  lazy val terminalStat: Parsley[Statement] = (returnStat <|> exit).label("return/exit_statement")
   val returnStat: Parsley[Statement] = Return(RETURN ~> expr)
   val exit: Parsley[Statement] = Exit(EXIT ~> expr)
 
