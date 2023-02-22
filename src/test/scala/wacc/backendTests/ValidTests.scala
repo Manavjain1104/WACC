0451ff7e6129f47686f6b8b5b32c2a2ca2f63cd8
Krish Maha <krish@mahas.net>
2023-02-22 17:40:01 +0000

    Use applyRecursively function in backend testing

--- src/test/scala/wacc/backendTests/ValidTests.scala
+++ src/test/scala/wacc/backendTests/ValidTests.scala
@@ -9,27 +9,28 @@ import scala.collection.mutable
 import scala.language.postfixOps
 import scala.io.Source
 import scala.collection.mutable.ListBuffer
+import scala.reflect.io.Directory
 import sys.process._
 import scala.util.control.Breaks.break
 
 class ValidTests extends AnyFlatSpec {
 
-//  def applyRecursively(dir: String, fn: (File) => Any) {
-//    def listAndProcess(dir: File) {
-//      dir.listFiles match {
-//        case null => out.println("exception: dir cannot be listed: " + dir.getPath); List[File]()
-//        case files => files.toList.sortBy(_.getName).foreach(file => {
-//          fn(file)
-//          if (!java.nio.file.Files.isSymbolicLink(file.toPath) && file.isDirectory) listAndProcess(file)
-//        })
-//      }
-//    }
-//    listAndProcess(new File(dir))
-//  }
+  def applyRecursively(dir: String, fn: (File) => Any) {
+    def listAndProcess(dir: File) {
+      dir.listFiles match {
+        case null => out.println("exception: dir cannot be listed: " + dir.getPath); List[File]()
+        case files => files.toList.sortBy(_.getName).foreach(file => {
+          fn(file)
+          if (!java.nio.file.Files.isSymbolicLink(file.toPath) && file.isDirectory) listAndProcess(file)
+        })
+      }
+    }
+    listAndProcess(new File(dir))
+  }
 
 
   //want this to get expected output from .wacc file and compare it to output of our compiler.
-  def exampleFn(file: File): String = {
+  def exampleFn(file: File): Unit = {
     val source = Source.fromFile(file)
     val lb = ListBuffer[String]()
     val out = ListBuffer[String]()
@@ -68,7 +69,12 @@ class ValidTests extends AnyFlatSpec {
     for (x <- out) {
       s ++= x
     }
-    s.toString()
+    //val bashOutput = s"./compile $file" !!
+    val bashOutput = "correct\n"
+    if (s.toString() != bashOutput) {
+      fail("Wrong output")
+    }
+
 
   }
 
@@ -76,26 +82,29 @@ class ValidTests extends AnyFlatSpec {
 
   behavior of "a valid if test"
   it should "succeed with exit code 0" in {
+    applyRecursively("src/test/scala/wacc/valid/if/", exampleFn)
     //    val f = "src/test/scala/wacc/valid/if/if1.wacc"
     //
     //    val s = exampleFn(new File(f))
 
     //    println(s)
-    val directory = new File("src/test/scala/wacc/valid/if")
-
-
-    if (directory.exists && directory.isDirectory) {
-      for (file <- directory.listFiles()) {
-        val expectedOutput = exampleFn(file)
-        //println(expectedOutput)
-        val bashOutput = s"./compile $file" !!
+//    val directory = new File("src/test/scala/wacc/valid/if/")
+//
+//
+//    if (directory.exists && directory.isDirectory) {
+//      for (file <- directory.listFiles()) {
+//        //val file = new File("/src/test/scala/wacc/valid/if/if6.wacc")
+//        val expectedOutput = exampleFn(file)
+//        println(expectedOutput)
+        //val bashOutput = s"./compile $file" !!
 
         //println(file)
         //println(expectedOutput)
         //        println(s.toString)
-        assert(expectedOutput == bashOutput)
-      }
-    }
+
+        //assert(expectedOutput == bashOutput)
+//      }
+//    }
 
 
 
