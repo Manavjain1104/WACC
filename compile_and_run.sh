cb85ac15054389355ff18a0dd97a35e79b143a3b
rt921 <rt921@ic.ac.uk>
2023-02-24 13:00:39 +0000

    fixed compile script for IO test

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,7 +1,14 @@
-#!bin/bash
+# shellcheck disable=SC2068
+java -jar wacc-25-compiler.jar $@
+
+ecode=$?
+if [ $ecode -ne 0 ]
+then
+    exit $ecode
+fi
 
 fname=$(basename "$1" .wacc)
-
-[ ! -e ${fname}.s ] && exit 1
 arm-linux-gnueabi-gcc -o "${fname}" -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "${fname}.s"
 qemu-arm -L /usr/arm-linux-gnueabi/ "${fname}"
+mv ${fname} build
+exit $?
