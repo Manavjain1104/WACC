cae6c6784c581d89c0b70651d30e645a287a8fc6
Krish Maha <krish@mahas.net>
2023-03-16 00:08:25 +0000

    Fix / error in front of dirs in backend testing script

--- src/test/scala/wacc/backendTests/ValidTests.scala
+++ src/test/scala/wacc/backendTests/ValidTests.scala
@@ -113,13 +113,13 @@ class ValidTests extends AnyFlatSpec {
 
   behavior of "valid binarySortTree advanced test"
   it should "succeed with exit code 0" taggedAs(ValidTests) in {
-    exampleFn(new File("/src/test/scala/wacc/valid/advanced/binarySortTree.wacc"))
+    exampleFn(new File("src/test/scala/wacc/valid/advanced/binarySortTree.wacc"))
   }
 
   behavior of "valid array tests"
   it should "succeed with exit code 0" taggedAs(ValidTests) in {
     //applyRecursively("src/test/scala/wacc/valid/array/", exampleFn)
-    exampleFn(new File("/src/test/scala/wacc/valid/array/emptyArrayScope.wacc"))
+    exampleFn(new File("src/test/scala/wacc/valid/array/emptyArrayScope.wacc"))
   }
 
   behavior of "valid basic exit tests"
